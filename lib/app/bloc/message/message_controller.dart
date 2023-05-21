d20923280f88f2057bb0ffd64a8d8393ed261c44
renankanu <renankanu@gmail.com>
2023-05-21 19:20:06 -0300

    WIP

--- lib/app/bloc/message/message_controller.dart
+++ lib/app/bloc/message/message_controller.dart
@@ -1,11 +1,28 @@
+import 'dart:math';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'message_state.dart';
 
 class MessageController extends Cubit<MessageState> {
   MessageController() : super(MessageInitial());
+
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  CollectionReference collection =
+      FirebaseFirestore.instance.collection('motivational');
+
   Future<void> getMessage() async {
     emit(MessageLoading());
+    await getRandomMessage();
     emit(MessageSuccess('Hello World'));
+  }
+
+  Future<void> getRandomMessage() async {
+    QuerySnapshot querySnapshot = await collection.get();
+    List<DocumentSnapshot> documents = querySnapshot.docs;
+    int randomIndex = Random().nextInt(documents.length);
+    DocumentSnapshot randomDocument = documents[randomIndex];
+    print(randomDocument.data());
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -5,10 +6,14 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'app/bloc/message/message_controller.dart';
 import 'app/routes/app_routes.dart';
 import 'app/shared/theme/app_theme.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: '.env');
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(const MyApp());
 }
